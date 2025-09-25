a98457489fd9ab3040b1270bba9e6cc0690ee023
ram-from-tvl <ramfromtvl@gmail.com>
2025-09-25 15:35:37 +0530

    added some african countries

--- src/v1/data/get_solar_capacities.py
+++ src/v1/data/get_solar_capacities.py
@@ -64,6 +64,136 @@ manual_countries = {
             "Wikipedia (Ember 2024 data) - https://en.wikipedia.org/wiki/Solar_power_by_country"
         ),
     },
+    "BEN": {
+        "country_name": "Benin",
+        "capacity_gw": 0.13,
+        "source": "https://ember-energy.org/latest-insights/global-electricity-review-2025/",
+    },
+    "BFA": {
+        "country_name": "Burkina Faso",
+        "capacity_gw": 0.09,
+        "source": "https://ember-energy.org/latest-insights/global-electricity-review-2025/",
+    },
+    "BDI": {
+        "country_name": "Burundi",
+        "capacity_gw": 0.017,
+        "source": "https://ember-energy.org/latest-insights/global-electricity-review-2025/",
+    },
+    "CMR": {
+        "country_name": "Cameroon",
+        "capacity_gw": 0.09,
+        "source": "https://ember-energy.org/latest-insights/the-first-evidence-of-a-take-off-in-solar-in-africa/",
+    },
+    "CPV": {
+        "country_name": "Cape Verde",
+        "capacity_gw": 0.03,
+        "source": "https://ember-energy.org/latest-insights/global-electricity-review-2025/",
+    },
+    "CAF": {
+        "country_name": "Central African Republic",
+        "capacity_gw": 0.01,
+        "source": "https://ember-energy.org/latest-insights/the-first-evidence-of-a-take-off-in-solar-in-africa/",
+    },
+    "COM": {
+        "country_name": "Comoros",
+        "capacity_gw": 0.005,
+        "source": "https://www.ren21.net/gsr-2025/downloads/pdf/go/GSR_2025_GO_2025_Full_Report.pdf",
+    },
+    "DJI": {
+        "country_name": "Djibouti",
+        "capacity_gw": 0.02,
+        "source": "https://ember-energy.org/latest-insights/global-electricity-review-2025/",
+    },
+    "ERI": {
+        "country_name": "Eritrea",
+        "capacity_gw": 0.01,
+        "source": "https://ember-energy.org/latest-insights/the-first-evidence-of-a-take-off-in-solar-in-africa/",
+    },
+    "SWZ": {
+        "country_name": "Eswatini",
+        "capacity_gw": 0.03,
+        "source": "https://www.globalsolarcouncil.org/news/global-solar-council-africas-solar-market-set-to-surge-42-in-2025-but-finance-bottlenecks-threaten-growth/",
+    },
+    "GAB": {
+        "country_name": "Gabon",
+        "capacity_gw": 0.01,
+        "source": "https://ember-energy.org/latest-insights/the-first-evidence-of-a-take-off-in-solar-in-africa/",
+    },
+    "GMB": {
+        "country_name": "Gambia",
+        "capacity_gw": 0.03,
+        "source": "https://www.pv-magazine.com/2023/02/10/gambia-inaugurates-23-mw-pv-plant/",
+    },
+    "GNQ": {
+        "country_name": "Equatorial Guinea",
+        "capacity_gw": 0.005,
+        "source": "https://www.ren21.net/gsr-2025/downloads/pdf/go/GSR_2025_GO_2025_Full_Report.pdf",
+    },
+    "GIN": {
+        "country_name": "Guinea",
+        "capacity_gw": 0.01,
+        "source": "https://ember-energy.org/latest-insights/global-electricity-review-2025/",
+    },
+    "GNB": {
+        "country_name": "Guinea-Bissau",
+        "capacity_gw": 0.005,
+        "source": "https://www.ren21.net/gsr-2025/downloads/pdf/go/GSR_2025_GO_2025_Full_Report.pdf",
+    },
+    "CIV": {
+        "country_name": "Ivory Coast",
+        "capacity_gw": 0.05,
+        "source": "https://ember-energy.org/latest-insights/global-electricity-review-2025/",
+    },
+    "LES": {
+        "country_name": "Lesotho",
+        "capacity_gw": 0.005,
+        "source": "https://ember-energy.org/latest-insights/global-electricity-review-2025/",
+    },
+    "LSO": {
+        "country_name": "Lesotho",
+        "capacity_gw": 0.005,
+        "source": "https://ember-energy.org/latest-insights/global-electricity-review-2025/",
+    },
+    "MRT": {
+        "country_name": "Mauritania",
+        "capacity_gw": 0.04,
+        "source": "https://ember-energy.org/latest-insights/global-electricity-review-2025/",
+    },
+    "MWI": {
+        "country_name": "Malawi",
+        "capacity_gw": 0.03,
+        "source": "https://www.ren21.net/gsr-2025/downloads/pdf/go/GSR_2025_GO_2025_Full_Report.pdf",
+    },
+    "STP": {
+        "country_name": "Sao Tome and Principe",
+        "capacity_gw": 0.005,
+        "source": "https://www.ren21.net/gsr-2025/downloads/pdf/go/GSR_2025_GO_2025_Full_Report.pdf",
+    },
+    "SLE": {
+        "country_name": "Sierra Leone",
+        "capacity_gw": 0.02,
+        "source": "https://ember-energy.org/latest-insights/the-first-evidence-of-a-take-off-in-solar-in-africa/",
+    },
+    "SOM": {
+        "country_name": "Somalia",
+        "capacity_gw": 0.02,
+        "source": "https://ember-energy.org/latest-insights/the-first-evidence-of-a-take-off-in-solar-in-africa/",
+    },
+    "SSD": {
+        "country_name": "South Sudan",
+        "capacity_gw": 0.01,
+        "source": "https://www.ren21.net/gsr-2025/downloads/pdf/go/GSR_2025_GO_2025_Full_Report.pdf",
+    },
+    "SYC": {
+        "country_name": "Seychelles",
+        "capacity_gw": 0.01,
+        "source": "https://ember-energy.org/latest-insights/global-electricity-review-2025/",
+    },
+    "TCD": {
+        "country_name": "Chad",
+        "capacity_gw": 0.02,
+        "source": "https://ember-energy.org/latest-insights/the-first-evidence-of-a-take-off-in-solar-in-africa/",
+    },
     "ALB": {
         "country_name": "Albania",
         "capacity_gw": 0.21,
